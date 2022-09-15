9708f3bf774977c3c65c011701841aec6d25b357
Olivier Lartillot <olivier.lartillot@gmail.com>
2022-09-15 17:52:34 +0200

    préparation de l'environnement js + initialisation d'un tableau d'objets articles

---
+++ js/articles.js
@@ -0,0 +1,22 @@
+let articles = [
+    {
+        title : "Un premier article",
+        content : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ipsam animi porro mollitia ipsum. Velit laboriosam tenetur hic laborum dolore maiores. Quisquam excepturi qui suscipit voluptas quidem distinctio, minima aperiam.Incidunt vel voluptas sunt nulla! Corrupti quis saepe eius quisquam labore magni dignissimos perferendis! Unde, ipsa labore consequatur voluptate deserunt mollitia vitae explicabo neque quo molestias veritatis adipisci, culpa architecto."
+    },
+    {
+        title : "Un deuxième article",
+        content : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ipsam animi porro mollitia ipsum. Velit laboriosam tenetur hic laborum dolore maiores. Quisquam excepturi qui suscipit voluptas quidem distinctio, minima aperiam.Incidunt vel voluptas sunt nulla! Corrupti quis saepe eius quisquam labore magni dignissimos perferendis! Unde, ipsa labore consequatur voluptate deserunt mollitia vitae explicabo neque quo molestias veritatis adipisci, culpa architecto."
+    },
+    {
+        title : "Un troisième article",
+        content : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ipsam animi porro mollitia ipsum. Velit laboriosam tenetur hic laborum dolore maiores. Quisquam excepturi qui suscipit voluptas quidem distinctio, minima aperiam.Incidunt vel voluptas sunt nulla! Corrupti quis saepe eius quisquam labore magni dignissimos perferendis! Unde, ipsa labore consequatur voluptate deserunt mollitia vitae explicabo neque quo molestias veritatis adipisci, culpa architecto."
+    },
+    {
+        title : "Un quatrième article",
+        content : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error ipsam animi porro mollitia ipsum. Velit laboriosam tenetur hic laborum dolore maiores. Quisquam excepturi qui suscipit voluptas quidem distinctio, minima aperiam.Incidunt vel voluptas sunt nulla! Corrupti quis saepe eius quisquam labore magni dignissimos perferendis! Unde, ipsa labore consequatur voluptate deserunt mollitia vitae explicabo neque quo molestias veritatis adipisci, culpa architecto."
+    },
+
+
+
+
+]
